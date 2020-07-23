0a01b5db79c6bd3746b8a9e1e9216bf333107cc3
Zack McKenna <zackmckenna@gmail.com>
2020-07-23 12:36:12 -0400

    added lint and box display

--- lock/src/App.js
+++ lock/src/App.js
@@ -1,17 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
 
 const App = () => {
-  useEffect(() => {
+  const [isLocked, setIsLocked] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [passwordInput, setPasswordInput ] = useState('')
 
-  })
+  useEffect(() => {
+    axios.get('http://localhost:3001/state')
+      .then(res => res.data)
+      .then(data => {
+        setIsLocked(data.isLocked)
+        setIsLoading(false)
+      })
+  }, [])
+
   return (
     <div className="App">
       <header>
         <h1>LockBox</h1>
         <div>
-
+          {isLoading && <p>loading . . .</p>}
+          {!isLoading && isLocked ? <h1>box is locked</h1> : <h1>box is unlocked</h1>}
         </div>
+        <input
+           value={passwordInput} placeholder='password' type='password'/>
       </header>
     </div>
   );
